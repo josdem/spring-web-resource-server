71a6dc4668daf705c53489b77543f5c487b6f5ac
josdem <joseluis.delacruz@gmail.com>
2023-10-01 09:35:48 -0400

    Fixing resource server configuration

--- src/main/java/com/josdem/web/resource/config/SecurityConfig.java
+++ src/main/java/com/josdem/web/resource/config/SecurityConfig.java
@@ -4,21 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebSecurity
-@EnableWebMvc
 public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.mvcMatcher("/categories/**")
-                .authorizeRequests()
-                .mvcMatchers("/categories/**")
-                .access("hasAuthority('SCOPE_categories.read')")
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-        return http.build();
-    }
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.mvcMatcher("/categories/**")
+        .authorizeRequests()
+        .mvcMatchers("/categories/**")
+        .access("hasAuthority('SCOPE_categories.read')")
+        .and()
+        .oauth2ResourceServer()
+        .jwt();
+    return http.build();
+  }
 }
